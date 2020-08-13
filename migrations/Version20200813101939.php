a6b07479b62a2c4edafc3f30806658dd4a591ee6
mwalczak <mateusz@walczaki.com>
2020-08-13 12:20:27 +0200

    [WIP] issue | project relation fixed migration

---
+++ migrations/Version20200813101939.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200813101939 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE issue ADD project_id INT NOT NULL');
+        $this->addSql('ALTER TABLE issue ADD CONSTRAINT FK_12AD233E166D1F9C FOREIGN KEY (project_id) REFERENCES project (id)');
+        $this->addSql('CREATE INDEX IDX_12AD233E166D1F9C ON issue (project_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE issue DROP FOREIGN KEY FK_12AD233E166D1F9C');
+        $this->addSql('DROP INDEX IDX_12AD233E166D1F9C ON issue');
+        $this->addSql('ALTER TABLE issue DROP project_id');
+    }
+}
